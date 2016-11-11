09c0f67f76981993eabe099c24a53d31011ae9ee
Stefan Guggisberg <guggs2@bfh.ch>
2016-11-11 09:42:39 +0100

    Menü als Template

--- src/ch/bfh/guggisberg/stefan/beans/LoginBean.java
+++ src/ch/bfh/guggisberg/stefan/beans/LoginBean.java
@@ -6,6 +6,8 @@ import java.math.BigInteger;
 import javax.annotation.ManagedBean;
 import javax.annotation.Resource;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
@@ -38,12 +40,12 @@ public class LoginBean implements Serializable {
 		System.out.println("Prüfe Login");
 		System.out.println("Email: "+user.getUserEmail());
 		System.out.println("Passwort: "+user.getPasswords()); // Darf nie so benutzt werden. Ist rein als Schulprojekt!
-	
-//		if((user==null) || (user.getUserEmail()==null) || (user.getPasswords()==null) ){ 
-//			System.out.println("Userobjekt ist null oder propertys davon");
-//			return "login";
+
+		//		if((user==null) || (user.getUserEmail()==null) || (user.getPasswords()==null) ){ 
+		//			System.out.println("Userobjekt ist null oder propertys davon");
+		//			return "login";
 		// Sollte nicht notwendig sein. 
-//		}
+		//		}
 		//Query query = em.createNativeQuery("Select userid from user u WHERE useremail='" + user.getUserEmail()+ "' AND userpassword='"+user.getUserPassword()+"'");
 		// Query query = em.createNativeQuery("Select count(*) from user u WHERE u.useremail='" + user.getUserEmail()+ "'");
 		Query query = em.createNativeQuery("Select userid from user u WHERE u.useremail='" + user.getUserEmail()+ "'");
@@ -54,11 +56,32 @@ public class LoginBean implements Serializable {
 			user = em.find(User.class, result.longValue());
 			System.out.println("User " +user.getUserName() + " hat eingeloggt");
 			loggedIn=true;
+			System.out.println("Login Status: " + loggedIn);
 			return "/secured/welcome";
 		}else{
 			System.out.println("Nicht eingeloggt");
 		}
+
+		// Set login ERROR
+//		FacesMessage msg = new FacesMessage("Login error!", "ERROR MSG");
+//		msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+//		FacesContext.getCurrentInstance().addMessage(null, msg);
+
 		return "login";
+	}
+
+	public String doLogout(){
+
+
+		// Set the paremeter indicating that user is logged in to false
+		loggedIn = false;
+		// Set logout message
+//		FacesMessage msg = new FacesMessage("Logout success!", "INFO MSG");
+//		msg.setSeverity(FacesMessage.SEVERITY_INFO);
+//		FacesContext.getCurrentInstance().addMessage(null, msg);
+
+
+		return "index";
 	}
 	public boolean isLogged(){
 		return loggedIn;
